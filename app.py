415404445307140511521e6eb560c10e816598da
Chris <christianOlmos@protonmail.com>
2020-04-15 23:38:56 +0000

    quick class test on python

---
+++ app.py
@@ -0,0 +1,12 @@
+import random
+
+who = ['the dog','my granma','his turtle','my bird']
+what = ['eat','pissed','crushed','broked']
+when = ['before the class','right in time','when I finished','during my lunch','while I was praying']
+
+who1 = random.choice(who)
+what2 = random.choice(what)
+when3 = random.choice(when) 
+
+print(who1 +' '+ what2 +' '+ when3)
+
